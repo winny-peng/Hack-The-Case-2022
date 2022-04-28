9270d48fd3ffa4c2c8a69778cebeb41658a954ce
Winny Peng <72108128+pengwinny@users.noreply.github.com>
2022-04-27 23:50:10 -0400

    Tree grows now.

--- js/custom.js
+++ js/custom.js
@@ -3,8 +3,28 @@
 var point_count = 0;
 
 function point_add() {
-	point_count += 10;
-	let tree = document.getElementById('tree');
-	tree.style.height = point_count;
-	location.reload();
+	// variables
+	var tree_state = 1;
+	
+	//increment points
+	point_count += 100;
+	
+	if (point_count == 0) {
+		tree_state = 1;
+	} 
+	else if (point_count == 100) {
+		tree_state = 2;
+	} 
+	else if (point_count == 200) {
+		tree_state = 3;
+	} 
+	else if (point_count == 200) {
+		tree_state = 4;
+	} 
+	else {
+		tree_state = 5;
+	}
+	
+	var src = 'images/tree-' + tree_state + '.jpg';
+	document.getElementById('tree').src=src;
 }	
