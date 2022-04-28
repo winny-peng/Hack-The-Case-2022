6aaedea487a62d80d087d5683139ca97ccee4985
Winny Peng <72108128+pengwinny@users.noreply.github.com>
2022-04-28 10:44:45 -0400

    Added tree modal popup.

--- js/custom.js
+++ js/custom.js
@@ -1,30 +1,28 @@
 // JavaScript Document
 
+// === GLOBAL VARIABLES ===
 var point_count = 0;
 
+const tree_image = document.getElementById("tree");
+tree_image.src = "images/tree-1.jpg";
+
 function point_add() {
-	// variables
-	var tree_state = 1;
-	
 	//increment points
 	point_count += 100;
 	
 	if (point_count == 0) {
-		tree_state = 1;
+		tree_image.src = "images/tree-1.jpg";
 	} 
 	else if (point_count == 100) {
-		tree_state = 2;
+		tree_image.src = "images/tree-2.jpg";
 	} 
 	else if (point_count == 200) {
-		tree_state = 3;
+		tree_image.src = "images/tree-3.jpg";
 	} 
 	else if (point_count == 300) {
-		tree_state = 4;
+		tree_image.src = "images/tree-4.jpg";
 	} 
 	else {
-		tree_state = 5;
+		tree_image.src = "images/tree-5.jpg";
 	}
-	
-	var src = 'images/tree-' + tree_state + '.jpg';
-	document.getElementById('tree').src=src;
 }	
