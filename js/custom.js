50d8eb5f30728dbc31d54711d4c5804513231acc
Winny Peng <72108128+pengwinny@users.noreply.github.com>
2022-04-28 22:20:32 -0400

    Added stats calculation functions.

--- js/custom.js
+++ js/custom.js
@@ -32,13 +32,29 @@ function point_add() {
 	
 	var src = 'images/tree-' + tree_state + '.jpg';
 	var point_total = 'Eco Points: ' + point_count;
+	
+	// update numbers
 	document.getElementById('tree').src=src;
 	document.getElementById('point_display_value').innerHTML=point_total;
+	document.getElementById('points_stats_points').innerHTML=calc_trees_rewards();
+	document.getElementById('points_stats_trees').innerHTML=calc_trees_planted();
+	document.getElementById('points_stats_money').innerHTML=point_count;
 	purchase();
 }	
 
+// complete purchase
 function purchase() {
   var x = document.getElementById("purchase_toast");
   x.className = "show";
   setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+// calculate the number of trees Amazon planted on the user's behalf (1 tree = 1000 leaves)
+function calc_trees_planted() {
+	return Math.floor(point_count/1000);
+}
+
+// calculate the cashback value 
+function calc_trees_rewards() {
+	return "$" + (point_count/10000).toFixed(2);
 }
